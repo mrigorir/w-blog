82a23cf9acf54d9bb719ce7d4962acf3b5bfbbb8
Marco <marcoparra311@gmail.com>
2020-12-21 14:41:51 -0400

    Jimmy blog's project

---
+++ wp-content/uploads/mc4wp-debug-log.php
@@ -0,0 +1,3 @@
+<?php exit; ?>
+[2020-10-29 22:49:10] WARNING: Form 159 > marc*********@gm***.com is already subscribed to the selected list(s)
+[2020-10-29 23:00:05] WARNING: Form 159 > marc*********@gm***.com is already subscribed to the selected list(s)
